3951d3db76886f9899070f1b32e078ed553405d3
LordMungi <lrdmungi@gmail.com>
2025-11-10 21:35:36 -0300

    fixed other bat file issue

--- scripts/copyFiles.bat
+++ scripts/copyFiles.bat
@@ -1,32 +1,25 @@
-echo -Running inital configuration-
+echo -Running initial configuration-
 
-set path=%~1
-set out=%~2
+set "src=%~1"
+set "out=%~2"
 
-echo using %path%
+echo using %src%
 echo and %out%
 
-cd %path%
+cd "%src%"
 
 echo step 1
 
-if not exist %out%\ (
-	echo copying library dependencies at
-	echo %path%lib\
-	copy "%path%lib\sigil-vc2015-win64\lib\*.dll" "%out%" /y
+    echo copying library dependencies at
+    echo "%src%\lib\"
+    xcopy "%src%\lib\sigil-vc2015-win64\lib\*.dll" "%out%\" /y
 
-	echo creating resource folder at
-	echo %path%res\
-	echo audio
-	copy "%path%res\audio\" "%out%res\audio\" /y
-	echo fonts	
-	copy "%path%res\fonts\" "%out%res\fonts\" /y
-	echo sprites
-	copy "%path%res\sprites\" "%out%res\sprites\" /y
+    echo creating resource folder at
+    echo "%src%\res\"
+    xcopy "%src%\res\*" "%out%\res\" /y /s /i
 
-	echo crating configs and data
-	copy "%path%config\" "%out%config\" /y
-	copy "%path%data\" %out%data\" /y
-)
+    echo creating configs and data
+    xcopy "%src%\config\*" "%out%\config\" /y /s /i
+    xcopy "%src%\data\*"   "%out%\data\"   /y /s /i
 
 echo Done!
